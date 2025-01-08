c156cc4aa79984eadc39ad9b776b5001a5c69b5e
Pintu Singh <pintusingh150@gmail.com>
2025-01-08 22:27:25 +0000

    adding functions to read cpu, memory and process info

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hellooo.. from main..")
+	fmt.Println("This is the entry point of the program")
+
+	cpuUsage := GetCpuUsage()
+	memUsage := GetMemUsage()
+	pInfo := GetRunningProcesses()
+
+	fmt.Println("CPU Usage: ", cpuUsage)
+	fmt.Println("Memory Usage: ", memUsage)
+	fmt.Println("Running Processes: ", pInfo)
 }
